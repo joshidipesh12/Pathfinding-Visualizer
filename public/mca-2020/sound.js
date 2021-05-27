ae0284195ced493671367ceef768da23c6e7c92f
DipeshJ <joshidipesh122@gmail.com>
2021-05-28 00:14:24 +0530

    sound fix

--- public/mca-2020/sound.js
+++ public/mca-2020/sound.js
@@ -2,7 +2,7 @@ var button_sound = document.getElementById("actualStartButton");
 // console.log(button_sound)
 button_sound.addEventListener("click", function () {
   var audio_sound = new Audio();
-  audio_sound.src = "./sound.mp4";
+  audio_sound.src = "sound.mp3";
   audio_sound.loop = true;
   audio_sound.play();
 });
